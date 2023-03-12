478f8e300f3206ecc61e32ac1aca7b4b75b80242
Trent Brown <trent.brown@leadingedje.com>
2023-03-12 10:57:44 -0400

    initial commit

---
+++ src/main/java/org/springframework/samples/petclinic/vet/VetService.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.vet;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class VetService {
+
+	@Autowired
+	VetRepository vetRepository;
+
+	public Vet createVet(String firstName, String lastName, List<Specialty> specialties) {
+		Vet vet = new Vet();
+		vet.setFirstName(firstName);
+		vet.setLastName(lastName);
+		specialties.forEach(vet::addSpecialty);
+
+		return vetRepository.save(vet);
+	}
+}
